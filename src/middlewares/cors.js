b61960e56c7e977820346aa80806895b72a2ea17
ScxttM <alan.montesinos@uabc.edu.mx>
2024-08-02 02:20:44 -0700

    changed to export default

--- src/middlewares/cors.js
+++ src/middlewares/cors.js
@@ -2,7 +2,7 @@ import cors from "cors";
 
 const ACCEPTED_ORIGINS = ["http://localhost:8080", "http://localhost:3000"];
 
-export const corsMiddleware = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) =>
+const corsMiddleware = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) =>
   cors({
     origin: (origin, callback) => {
       if (acceptedOrigins.includes(origin)) {
@@ -16,3 +16,5 @@ export const corsMiddleware = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) =>
       return callback(new Error("Not allowed by CORS"));
     },
   });
+
+export default corsMiddleware;
