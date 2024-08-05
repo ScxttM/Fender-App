7d981d1530f477c76081823c6e9f0a5424b7e1e1
ScxttM <alan.montesinos@uabc.edu.mx>
2024-08-05 04:10:26 -0700

    cached pokemon

--- src/app.js
+++ src/app.js
@@ -1,8 +1,13 @@
 import express, { json } from "express";
 import corsMiddleware from "./middlewares/cors.js";
 import verifyToken from "./middlewares/auth.js";
-import userRouter from "./routes/user.js";
+import createUserRouter from "./routes/user.js";
+import favoritesRouter from "./routes/favorites.js";
 import "dotenv/config";
+import axios from "axios";
+import NodeCache from "node-cache";
+
+const cache = new NodeCache();
 
 const PORT = process.env.PORT || 3000;
 
@@ -15,10 +20,54 @@ const createApp = () => {
   app.get("/", (req, res) => {
     res.json({ message: "API is working" });
   });
-  app.post("/login", userRouter);
-  app.post("/logout", userRouter);
-  app.post("/register", userRouter);
-  app.use("/users", verifyToken, userRouter);
+  app.post("/login", createUserRouter(cache));
+  app.post("/logout", createUserRouter(cache));
+  app.post("/register", createUserRouter(cache));
+  app.use("/users", verifyToken, createUserRouter(cache));
+  app.use("/favorites", verifyToken, favoritesRouter);
+
+  app.get("/pokemon", async (req, res) => {
+    const page = req.query.page || 1;
+    const offset = (page - 1) * 20;
+    const url = `https://pokeapi.co/api/v2/pokemon?limit=20&offset=${offset}`;
+
+    const data = [];
+    let pokemonList = cache.get(url);
+
+    if (!pokemonList) {
+      const response = await axios.get(url);
+      pokemonList = response.data;
+      cache.set(url, pokemonList, 24 * 60 * 60); // 24 hours
+    }
+
+    for (const pokemon of pokemonList.results) {
+      let pokemonData = cache.get(pokemon.url);
+
+      if (!pokemonData) {
+        const response = await axios.get(pokemon.url);
+        pokemonData = {
+          id: response.data.id,
+          image: response.data.sprites.front_default,
+          types: response.data.types.map((type) => type.type.name),
+        };
+
+        cache.set(pokemon.url, pokemonData, 24 * 60 * 60); // 24 hours
+      }
+
+      pokemonData.name = pokemon.name;
+
+      data.push(pokemonData);
+    }
+
+    res.json({
+      info: {
+        count: pokemonList.count,
+        next: pokemonList.next,
+        prev: pokemonList.previous,
+      },
+      results: data,
+    });
+  });
 
   app.listen(PORT, () => {
     console.log(
