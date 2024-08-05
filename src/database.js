db8a17b42ee62b1200047920cacc8a708698dca8
ScxttM <alan.montesinos@uabc.edu.mx>
2024-08-05 08:55:33 -0700

    db host

--- src/database.js
+++ src/database.js
@@ -2,7 +2,7 @@ import mysql from "mysql2/promise";
 import "dotenv/config";
 
 const DEFAULT_CONFIG = {
-  host: "localhost",
+  host: process.env.DB_HOST,
   user: process.env.DB_USER,
   port: 3306,
   password: process.env.DB_PASSWORD,
