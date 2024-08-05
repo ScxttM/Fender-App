fcaeeaa26140acc2aa044bee95acd0eacd730c47
ScxttM <alan.montesinos@uabc.edu.mx>
2024-08-05 00:20:48 -0700

    user data trim

--- src/schemas/user.schema.js
+++ src/schemas/user.schema.js
@@ -1,11 +1,13 @@
 import z from "zod";
 
 const userSchema = z.object({
-  name: z.string({
-    invalid_type_error: "User name must be a string",
-    required_error: "User name is required.",
-  }),
-  email: z.string().email({
+  name: z
+    .string({
+      invalid_type_error: "User name must be a string",
+      required_error: "User name is required.",
+    })
+    .trim(),
+  email: z.string().trim().email({
     invalid_type_error: "User email must be a string",
     required_error: "User email is required.",
     message: "User email must be a valid email address",
@@ -15,6 +17,7 @@ const userSchema = z.object({
       invalid_type_error: "User password must be a string",
       required_error: "User password is required.",
     })
+    .trim()
     .min(6, {
       message: "User password must have at least 6 characters",
     }),
